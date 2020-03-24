9b56035a1d9e087ab412d9940792f19099b28221
Udara Alwis <udara.robotics@gmail.com>
2020-03-24 18:44:54 +0800

    init

---
+++ XFShellTemplateDemoApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XFShellTemplateDemoApp.Models;
+
+namespace XFShellTemplateDemoApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
